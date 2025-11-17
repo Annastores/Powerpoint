601ab77938b1c293caa1b20936c17d1c78fd45da
Annastores <eduardgevorgyan2007@gmail.com>
2025-11-17 10:25:26 +0400

    Initial commit2

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def set_slide():
 def control():
     return render_template("control.html")
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
